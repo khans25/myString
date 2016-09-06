8400cb9e977cfbf79deb1f390bd2aea78d00faea
khans25 <khans25@vcu.edu>
2016-09-06 18:38:19 -0400

    added string ex.

---
+++ myString/myString.cpp
@@ -0,0 +1,17 @@
+// myString.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+	char name[50];
+	cout << "enter a name" << endl;
+	gets_s(name);
+	cout << "your name is " << name << endl;
+    return 0;
+}
+
